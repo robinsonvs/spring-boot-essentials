8882c0683281d8af00821919ec14c9435821b06b
robinsonvs <robinsonvs@gmail.com>
2020-07-18 10:37:00 -0300

    created post mapping

--- src/main/java/com/severo/demospring/controller/StudentController.java
+++ src/main/java/com/severo/demospring/controller/StudentController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
@@ -41,5 +38,10 @@ public class StudentController {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Student not found"));
 
         return ResponseEntity.ok(studentFound);
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> save(@RequestBody Student student) {
+        return ResponseEntity.ok(studentRepository.save(student));
     }
 }
--- src/main/java/com/severo/demospring/repository/StudentRepository.java
+++ src/main/java/com/severo/demospring/repository/StudentRepository.java
@@ -4,12 +4,27 @@ package com.severo.demospring.repository;
 import com.severo.demospring.domain.Student;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Repository
 public class StudentRepository {
+    private static List<Student> students;
+    static {
+        students = new ArrayList<>(List.of(
+                new Student(1, "Joao"),
+                new Student(2, "Maria"),
+                new Student(3, "Pedro")));
+    }
 
     public List<Student> listAll() {
-        return List.of(new Student(1, "Joao"), new Student(2, "Maria"), new Student(3, "Pedro"));
+        return students;
+    }
+
+    public Student save(Student student) {
+        student.setId(ThreadLocalRandom.current().nextInt(4, 100000));
+        students.add(student);
+        return student;
     }
 }
