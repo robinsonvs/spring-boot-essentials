1763fb42d6afff8b53121fa52e7e5b64e0e14350
robinsonvs <robinsonvs@gmail.com>
2020-07-18 13:26:44 -0300

    created spring data jpa mysql

--- src/main/java/com/severo/demospring/domain/Student.java
+++ src/main/java/com/severo/demospring/domain/Student.java
@@ -2,11 +2,21 @@ package com.severo.demospring.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
 
