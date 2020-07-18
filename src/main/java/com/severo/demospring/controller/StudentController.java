5e6e6eb1a3651de3c191e6ceaa8fe02eeff34c9b
robinsonvs <robinsonvs@gmail.com>
2020-07-18 10:17:26 -0300

    ajusted get controller

--- src/main/java/com/severo/demospring/controller/StudentController.java
+++ src/main/java/com/severo/demospring/controller/StudentController.java
@@ -8,8 +8,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -27,5 +29,17 @@ public class StudentController {
     public ResponseEntity<List<Student>> listAll() {
         log.info("Date formatted {}", dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return ResponseEntity.ok(studentRepository.listAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Student> findById(@PathVariable int id) {
+
+        Student studentFound = studentRepository.listAll()
+                .stream()
+                .filter(student -> student.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Student not found"));
+
+        return ResponseEntity.ok(studentFound);
     }
 }
